e1c53ffd86f53496a742de3327edea50f5aa319c
Jeff Tanner <jeff00seattle@gmail.com>
2018-05-15 08:59:43 -0700

    [PFD-2] fix dates_month_first_last

--- examples/example_dates_month_first_last.py
+++ examples/example_dates_month_first_last.py
@@ -17,3 +17,11 @@ pprint(month_last_date)
 month_first_date, month_last_date = pyfortified_dateutil.dates_month_first_last(dt.date(2017, 2, 3))
 pprint(month_first_date)
 pprint(month_last_date)
+
+month_first_date, month_last_date = pyfortified_dateutil.dates_month_first_last("2017-03", "%Y-%m")
+pprint(month_first_date)
+pprint(month_last_date)
+
+month_first_date, month_last_date = pyfortified_dateutil.dates_month_first_last("2017-03-02", "%Y-%m-%d")
+pprint(month_first_date)
+pprint(month_last_date)
